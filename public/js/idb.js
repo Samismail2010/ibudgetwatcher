f1dcbcc20154e633a871a3f834757bc0b6274db9
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-04-19 22:06:53 -0500

    add save function

--- public/js/idb.js
+++ public/js/idb.js
@@ -13,3 +13,29 @@ request.onupgradeneeded = function(event) {
     //create an object store (table) called `new_transaction` set it to have an auto incrementing primary key of sorts
     db.createObjectStore('new_transaction', {autoIncrement: true });
 };
+
+//upon a successful
+request.onsuccess = function(event) {
+    //when db is successfully created with its object store or simply established a connection, save refrence to a db in global variable
+    db = event.target.result;
+
+    //check if app is online, if yes run uploadTransaction() function to send all local db data to api
+    if(navigatior.onLine){
+        //uploadTransactions();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode)
+};
+
+//this  function will be executed if we attempt to submit a new transaction and there's no internet connection
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    //access the object store for 'new_transaction'
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    //add record to your store with add method
+    budgetObjectStore.add(record);
+}
